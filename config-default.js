7effb0b1556cd32efd5a4d68a486f75ddb6346ce
KasimYe <lip8658@live.com>
2019-11-20 22:34:53 +0800

    first add

---
+++ config-default.js
@@ -0,0 +1,10 @@
+var config = {
+    dialect: 'mysql',
+    database: 'misdb',
+    username: 'root',
+    password: 'BRYY@abc123',
+    host: 'localhost',
+    port: 3306
+};
+
+module.exports = config;
---
+++ controllers/index.js
@@ -0,0 +1,36 @@
+const entity = require('./../model');
+let User = entity.User;
+
+var fn_index = async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+};
+
+var fn_signin = async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === '' || password === '') {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    } else {        
+        var user = await User.create({
+            name: name,
+            gender: false,
+            email: name + '-' + Date.now() + '@garfield.pet',
+            passwd: password
+        });
+        console.log('created: ' + JSON.stringify(user));
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    }
+};
+
+module.exports = {
+    'GET /': fn_index,
+    'POST /signin': fn_signin
+};
